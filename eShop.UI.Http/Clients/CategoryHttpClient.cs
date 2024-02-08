f6d0c6e81b22f089c569089335885e7875f29b34
leobrowaldh <leobrowaldh@gmail.com>
2024-02-08 16:49:34 +0100

    added Ui services, ready to change to correct blazor type of ui project...

---
+++ eShop.UI.Http/Clients/CategoryHttpClient.cs
@@ -0,0 +1,13 @@
+﻿namespace eShop.UI.Http.Clients;
+
+public class CategoryHttpClient
+{
+	private readonly HttpClient _httpClient;
+
+	public CategoryHttpClient(HttpClient httpClient)
+    {
+		string _baseAddress = "https://localhost:5000/api/";
+		_httpClient = httpClient;
+		_httpClient.BaseAddress = new Uri($"{_baseAddress}categorys");
+	}
+}
---
+++ eShop.UI/Services/UIService.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace eShop.UI.Services;
+
+public class UIService(CategoryHttpClient categoryHttpClient)
+{
+	List<CategoryGetDTO> Categories { get; set; } = [];
+	public List<LinkGroup> CaregoryLinkGroups { get; private set; } =
+	[
+		new LinkGroup 
+		{ 
+			Name = "Categories",
+			LinkOptions = new List<LinkOption>()
+			{	//test data:
+			 	new LinkOption {Id = 1, Name = "Woman", IsSelected = true}, //test data
+			 	new LinkOption {Id = 2, Name = "Men", IsSelected = false},
+			 	new LinkOption {Id = 3, Name = "Children", IsSelected = false}
+			}
+		}
+	];
+	public int CurrentCategoryId { get; set; }
+}
